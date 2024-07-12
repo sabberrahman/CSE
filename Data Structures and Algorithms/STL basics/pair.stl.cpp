5a47234515c1643213439c447deb8d3e55dd7f23
sabberrahman <sabbersr1@gmail.com>
2024-07-12 22:42:54 +0600

    stl basics

---
+++ Data Structures and Algorithms/STL basics/pair.stl.cpp
@@ -0,0 +1,14 @@
+// pair contain only 2
+
+void Pair (){
+    pair<int,int> p={1,7} // decare datatypes
+    cout<< p.first <<"" << p/second //1 7
+
+    pair<double,pair<int,int>> p={12.09,{43,87}}
+    cout<<p.second.second <<""<<p.second.first // 87 , 43
+
+    pair<int,int> p_arr[]={{1,44},{32,49},{33,99}}
+    cout<<p_arr[1].first //32 
+
+    p_arr[5]={{1,44},{32,49},{33,99},{2,32},{232,323},{12,34}}
+}
---
+++ Data Structures and Algorithms/STL basics/vector.stl.cpp
@@ -0,0 +1,19 @@
+
+//Use arrays when you know the exact size of your data at compile time and prioritize raw performance for small datasets.You cannot change the size of an array after it's created
+
+//Use vectors in most other cases for their flexibility, safety, and ease of use. Their dynamic size and automatic memory management make them a more robust and preferred choice for general-purpose data storage in C++.
+
+void Vector (){
+    vector<int> v;
+
+    vector.push_back(1)  //will add 1 in last ..even if limit is finished,,it will scale
+    vector.emplace.back(2)
+
+    vector<pair<int,int>> pair_vector;
+
+    vector<int> v(3,44) // 44 44 44 
+
+    vector <int> v(5)
+}
+
+
